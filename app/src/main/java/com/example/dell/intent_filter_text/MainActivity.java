79289ee0dd1ef630d71a6e9113088d2a14361371
Violet <xinbai16@student.hh.se>
2016-09-07 13:45:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/intent_filter_text/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.dell.intent_filter_text;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button_another);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction("startAnotherActivity");
+                intent.addCategory("android.intent.category.DEFAULT");
+               intent.setData(Uri.parse("http://www.mathtop.com.cn/homework"));
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:13800138000"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
